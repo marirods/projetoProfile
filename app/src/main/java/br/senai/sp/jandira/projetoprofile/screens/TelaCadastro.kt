55846ca97752846cf9f64934ddff55dc97d37068
MARIANE DA SILVA RODRIGUES <24122311@dsn127m1249389.cfp127aluno.local>
2025-05-06 16:21:49 -0300

    segunda atualizacao

--- app/src/main/java/br/senai/sp/jandira/projetoprofile/screens/TelaCadastro.kt
+++ app/src/main/java/br/senai/sp/jandira/projetoprofile/screens/TelaCadastro.kt
@@ -15,8 +15,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -46,23 +53,24 @@ fun TelaCadastro() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(390.dp),
-                shape = RoundedCornerShape( 0.dp, 0.dp, 32.dp, 32.dp
+                shape = RoundedCornerShape(
+                    0.dp, 0.dp, 32.dp, 32.dp
                 ),
                 colors = CardDefaults.cardColors(
                     containerColor = Color.Magenta
                 )
-            ){
+            ) {
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
-                ){
+                ) {
                     Image(
                         modifier = Modifier
                             .fillMaxSize(),
                         painter = painterResource(id = br.senai.sp.jandira.projetoprofile.R.drawable.background),
                         contentDescription = "",
                     )
-                    Column (
+                    Column(
                         modifier = Modifier
                             .fillMaxSize(),
                         horizontalAlignment = Alignment.CenterHorizontally
@@ -74,7 +82,7 @@ fun TelaCadastro() {
                             modifier = Modifier
                                 .padding(35.dp)
                         )
-                        Card (
+                        Card(
                             modifier = Modifier
                                 .size(100.dp),
                             shape = CircleShape,
@@ -89,20 +97,151 @@ fun TelaCadastro() {
                             ),
                             elevation = CardDefaults
                                 .cardElevation(2.dp)
-                        ){
+                        ) {
                             Image(
                                 painter = painterResource(br.senai.sp.jandira.projetoprofile.R.drawable.usuario),
                                 contentDescription = ""
                             )
-                            Column (
+                        }
+                        Text(
+                            text = stringResource(br.senai.sp.jandira.projetoprofile.R.string.nome_1),
+                            color = Color.White,
+                            fontSize = 24.sp,
+                            modifier = Modifier
+                                .padding(15.dp)
+                        )
+                        Text(
+                            text = stringResource(br.senai.sp.jandira.projetoprofile.R.string.profissao),
+                            color = Color(0xFFE1DFDF),
+                            fontSize = 15.sp,
+                            modifier = Modifier
+                                .padding(start = 2.dp)
+                        )
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 15.dp, vertical = 15.dp),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+                            Card(
                                 modifier = Modifier
-                                    .fillMaxSize(),
-                                horizontalAlignment = Alignment.CenterHorizontally
-                            ){
-                                Text(
-                                    text = stringResource(br.senai.sp.jandira.projetoprofile.R.string.nome_1),
-                                    color = Color.White
+                                    .height(70.dp)
+                                    .size(80.dp),
+                                colors = CardDefaults.cardColors(
+                                    containerColor = Color(color = 0x85FFFFFF)
                                 )
+                            ) {
+                                Column (
+                                    modifier = Modifier
+                                        .fillMaxSize(),
+                                    verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ){
+                                    Icon(
+                                        modifier = Modifier
+                                            .size(20.dp),
+                                        imageVector = Icons.Default.Email,
+                                        contentDescription = "",
+                                        tint = Color(color = 0xFFFFFFFF)
+                                    )
+                                    Text(
+                                        text = stringResource(br.senai.sp.jandira.projetoprofile.R.string.email_icon),
+                                        color = Color.White,
+                                        fontSize = 15.sp,
+                                        modifier = Modifier
+                                            .padding(4.dp)
+                                    )
+                                }
+                            }
+                            Card(
+                                modifier = Modifier
+                                    .height(70.dp)
+                                    .size(80.dp),
+                                colors = CardDefaults.cardColors(
+                                    containerColor = Color(color = 0x85FFFFFF)
+                                )
+                            ) {
+                                Column (
+                                    modifier = Modifier
+                                        .fillMaxSize(),
+                                            verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ){
+                                    Icon(
+                                        modifier = Modifier
+                                            .size(20.dp),
+                                        imageVector = Icons.Default.Call,
+                                        contentDescription = "",
+                                        tint = Color(color = 0xFFFFFFFF)
+                                    )
+                                    Text(
+                                        text = stringResource(br.senai.sp.jandira.projetoprofile.R.string.telefone_icon),
+                                        color = Color.White,
+                                        fontSize = 15.sp,
+                                        modifier = Modifier
+                                            .padding(4.dp)
+                                    )
+                                }
+                            }
+                            Card(
+                                modifier = Modifier
+                                    .height(70.dp)
+                                    .size(80.dp),
+                                colors = CardDefaults.cardColors(
+                                    containerColor = Color(color = 0x85FFFFFF)
+                                )
+                            ){
+                                Column (
+                                    modifier = Modifier
+                                        .fillMaxSize(),
+                                    verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ){
+                                    Icon(
+                                        modifier = Modifier
+                                            .size(20.dp),
+                                        imageVector = Icons.Default.DateRange,
+                                        contentDescription = "",
+                                        tint = Color(color = 0xFFFFFFFF)
+                                    )
+                                    Text(
+                                        text = stringResource(br.senai.sp.jandira.projetoprofile.R.string.whats_icon),
+                                        color = Color.White,
+                                        fontSize = 15.sp,
+                                        modifier = Modifier
+                                            .padding(4.dp)
+                                    )
+                                }
+                            }
+                            Card(
+                                modifier = Modifier
+                                    .height(70.dp)
+                                    .size(80.dp),
+                                colors = CardDefaults.cardColors(
+                                    containerColor = Color(color = 0x85FFFFFF)
+                                )
+                            ){
+                                Column (
+                                    modifier = Modifier
+                                        .fillMaxSize(),
+                                    verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ){
+                                    Icon(
+                                        modifier = Modifier
+                                            .size(20.dp),
+                                        imageVector = Icons.Default.FavoriteBorder,
+                                        contentDescription = "",
+                                        tint = Color(color = 0xFFFFFFFF)
+                                    )
+                                    Text(
+                                        text = stringResource(br.senai.sp.jandira.projetoprofile.R.string.estrela_icon),
+                                        color = Color.White,
+                                        fontSize = 15.sp,
+                                        modifier = Modifier
+                                            .padding(4.dp)
+                                    )
+                                }
                             }
                         }
                     }
